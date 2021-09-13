81335a1970c4600af40c3c30a3d1733444a1888b
Luiz Neto <luiz.neto@mzgroup.com>
2021-09-13 03:45:25 -0300

    improves

--- src/infraestructure/S3.py
+++ src/infraestructure/S3.py
@@ -11,7 +11,7 @@ class InfraestructureStorage:
     def get_obj(self, objects):
         for obj in objects:
              obj['url'] =self.s3.generate_presigned_url('get_object', ExpiresIn=3600, Params={'Bucket': self.bucket, 'Key': obj['filename']})
-        return obj
+        return objects
     
     def upload_obj(self, object):
         return self.s3.upload_fileobj(object, self.bucket,object.filename)
